ad7e2b5091ea3c6e4c64c785cebbe4e28b1682e1
RobertEyler <167407674+RobertEyler@users.noreply.github.com>
2024-11-20 19:01:43 +0800

    Update context.rs

--- programs/amm-cpi/src/context.rs
+++ programs/amm-cpi/src/context.rs
@@ -262,51 +262,51 @@ pub struct Withdraw<'info> {
 pub struct SwapBaseIn<'info> {
     /// CHECK: Safe. amm Account
     #[account(mut)]
-    pub amm: UncheckedAccount<'info>,
+    pub amm: AccountInfo<'info>,
     /// CHECK: Safe. Amm authority Account
     #[account(
         seeds = [b"amm authority"],
         bump,
     )]
-    pub amm_authority: UncheckedAccount<'info>,
+    pub amm_authority: AccountInfo<'info>,
     /// CHECK: Safe. amm open_orders Account
     #[account(mut)]
-    pub amm_open_orders: UncheckedAccount<'info>,
+    pub amm_open_orders: AccountInfo<'info>,
     /// CHECK: Safe. amm_coin_vault Amm Account to swap FROM or To,
     #[account(mut)]
-    pub amm_coin_vault: UncheckedAccount<'info>,
+    pub amm_coin_vault: AccountInfo<'info>,
     /// CHECK: Safe. amm_pc_vault Amm Account to swap FROM or To,
     #[account(mut)]
-    pub amm_pc_vault: UncheckedAccount<'info>,
+    pub amm_pc_vault: AccountInfo<'info>,
     /// CHECK: Safe.OpenBook program id
-    pub market_program: UncheckedAccount<'info>,
+    pub market_program: AccountInfo<'info>,
     /// CHECK: Safe. OpenBook market Account. OpenBook program is the owner.
     #[account(mut)]
-    pub market: UncheckedAccount<'info>,
+    pub market: AccountInfo<'info>,
     /// CHECK: Safe. bids Account
     #[account(mut)]
-    pub market_bids: UncheckedAccount<'info>,
+    pub market_bids: AccountInfo<'info>,
     /// CHECK: Safe. asks Account
     #[account(mut)]
-    pub market_asks: UncheckedAccount<'info>,
+    pub market_asks: AccountInfo<'info>,
     /// CHECK: Safe. event_q Account
     #[account(mut)]
-    pub market_event_queue: UncheckedAccount<'info>,
+    pub market_event_queue: AccountInfo<'info>,
     /// CHECK: Safe. coin_vault Account
     #[account(mut)]
-    pub market_coin_vault: UncheckedAccount<'info>,
+    pub market_coin_vault: AccountInfo<'info>,
     /// CHECK: Safe. pc_vault Account
     #[account(mut)]
-    pub market_pc_vault: UncheckedAccount<'info>,
+    pub market_pc_vault: AccountInfo<'info>,
     /// CHECK: Safe. vault_signer Account
     #[account(mut)]
-    pub market_vault_signer: UncheckedAccount<'info>,
+    pub market_vault_signer: AccountInfo<'info>,
     /// CHECK: Safe. user source token Account. user Account to swap from.
     #[account(mut)]
-    pub user_token_source: UncheckedAccount<'info>,
+    pub user_token_source: AccountInfo<'info>,
     /// CHECK: Safe. user destination token Account. user Account to swap to.
     #[account(mut)]
-    pub user_token_destination: UncheckedAccount<'info>,
+    pub user_token_destination: AccountInfo<'info>,
     /// CHECK: Safe. user owner Account
     #[account(mut)]
     pub user_source_owner: Signer<'info>,
@@ -319,51 +319,51 @@ pub struct SwapBaseIn<'info> {
 pub struct SwapBaseOut<'info> {
     /// CHECK: Safe. amm Account
     #[account(mut)]
-    pub amm: UncheckedAccount<'info>,
+    pub amm: AccountInfo<'info>,
     /// CHECK: Safe. Amm authority Account
     #[account(
         seeds = [b"amm authority"],
         bump,
     )]
-    pub amm_authority: UncheckedAccount<'info>,
+    pub amm_authority: AccountInfo<'info>,
     /// CHECK: Safe. amm open_orders Account
     #[account(mut)]
-    pub amm_open_orders: UncheckedAccount<'info>,
+    pub amm_open_orders: AccountInfo<'info>,
     /// CHECK: Safe. amm_coin_vault Amm Account to swap FROM or To,
     #[account(mut)]
-    pub amm_coin_vault: UncheckedAccount<'info>,
+    pub amm_coin_vault: AccountInfo<'info>,
     /// CHECK: Safe. amm_pc_vault Amm Account to swap FROM or To,
     #[account(mut)]
-    pub amm_pc_vault: UncheckedAccount<'info>,
+    pub amm_pc_vault: AccountInfo<'info>,
     /// CHECK: Safe. OpenBook program id
-    pub market_program: UncheckedAccount<'info>,
+    pub market_program: AccountInfo<'info>,
     /// CHECK: Safe. OpenBook market Account. OpenBook program is the owner.
     #[account(mut)]
-    pub market: UncheckedAccount<'info>,
+    pub market: AccountInfo<'info>,
     /// CHECK: Safe. bids Account
     #[account(mut)]
-    pub market_bids: UncheckedAccount<'info>,
+    pub market_bids: AccountInfo<'info>,
     /// CHECK: Safe. asks Account
     #[account(mut)]
-    pub market_asks: UncheckedAccount<'info>,
+    pub market_asks: AccountInfo<'info>,
     /// CHECK: Safe. event_q Account
     #[account(mut)]
-    pub market_event_queue: UncheckedAccount<'info>,
+    pub market_event_queue: AccountInfo<'info>,
     /// CHECK: Safe. coin_vault Account
     #[account(mut)]
-    pub market_coin_vault: UncheckedAccount<'info>,
+    pub market_coin_vault: AccountInfo<'info>,
     /// CHECK: Safe. pc_vault Account
     #[account(mut)]
-    pub market_pc_vault: UncheckedAccount<'info>,
+    pub market_pc_vault: AccountInfo<'info>,
     /// CHECK: Safe. vault_signer Account
     #[account(mut)]
-    pub market_vault_signer: UncheckedAccount<'info>,
+    pub market_vault_signer: AccountInfo<'info>,
     /// CHECK: Safe. user source token Account. user Account to swap from.
     #[account(mut)]
-    pub user_token_source: UncheckedAccount<'info>,
+    pub user_token_source: AccountInfo<'info>,
     /// CHECK: Safe. user destination token Account. user Account to swap to.
     #[account(mut)]
-    pub user_token_destination: UncheckedAccount<'info>,
+    pub user_token_destination: AccountInfo<'info>,
     /// CHECK: Safe. user owner Account
     #[account(mut)]
     pub user_source_owner: Signer<'info>,
